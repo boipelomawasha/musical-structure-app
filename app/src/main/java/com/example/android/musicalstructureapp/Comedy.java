f945368c22ac79e43511d0a072e7ffb0b6d39966
boipelomawasha <boipelo.mawasha@jumo.world>
2018-09-09 12:07:04 +0200

    added search activity

--- app/src/main/java/com/example/android/musicalstructureapp/Comedy.java
+++ app/src/main/java/com/example/android/musicalstructureapp/Comedy.java
@@ -9,23 +9,16 @@ import java.util.ArrayList;
 
 public class Comedy extends AppCompatActivity {
 
+    // Create private list of audiobooks
+    private ArrayList<AudioBook> audioBooks = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.audiobook_list_view);
 
-        // Create a list of audiobooks
-        ArrayList<AudioBook> audioBooks = new ArrayList<>();
-
         //Add audiobooks
-        audioBooks.add(new AudioBook("The Last Black Unicorn", "Tiffany Haddish", "6 hours 3 minutes", "December 2017"));
-        audioBooks.add(new AudioBook("How Not to Get Shot: And Other Advice From White People", "D. L. Hughley", "3 hours 42 minutes", "June 2018"));
-        audioBooks.add(new AudioBook("Scrappy Little Nobody", "Anna Kendrick", "6 hours 1 minute", "November 2016"));
-        audioBooks.add(new AudioBook("I Hate Myselfie", "Shane Dawson", "4 hours 8 minutes", "March 2015"));
-        audioBooks.add(new AudioBook("Dad Is Fat", "Jim Gaffigan", "5 hours 27 minutes", "May 2013"));
-        audioBooks.add(new AudioBook("Seriously Funny", "Kevin Hart", "1 hour", "July 2010"));
-        audioBooks.add(new AudioBook("My Fake Problems", "David Spade", "1 hour 4 minutes", "May 2014"));
-        audioBooks.add(new AudioBook("Lies Chelsea Handler Told Me", "Various Readers", "6 hours 46 minutes", "May 2011"));
+        addAudiobooks();
 
         // Create instance AudioBook adapter with the ArrayList and context
         AudioBookAdapter myAdapter = new AudioBookAdapter(this, audioBooks);
@@ -36,6 +29,25 @@ public class Comedy extends AppCompatActivity {
         // MAke use of the adapter in your ListView instance
         listView.setAdapter(myAdapter);
 
+    }
+
+    private void addAudiobooks() {
+
+        audioBooks.add(new AudioBook("The Last Black Unicorn", "Tiffany Haddish", "6 hours 3 minutes", "December 2017"));
+        audioBooks.add(new AudioBook("How Not to Get Shot: And Other Advice From White People", "D. L. Hughley", "3 hours 42 minutes", "June 2018"));
+        audioBooks.add(new AudioBook("Scrappy Little Nobody", "Anna Kendrick", "6 hours 1 minute", "November 2016"));
+        audioBooks.add(new AudioBook("I Hate Myselfie", "Shane Dawson", "4 hours 8 minutes", "March 2015"));
+        audioBooks.add(new AudioBook("Dad Is Fat", "Jim Gaffigan", "5 hours 27 minutes", "May 2013"));
+        audioBooks.add(new AudioBook("Seriously Funny", "Kevin Hart", "1 hour", "July 2010"));
+        audioBooks.add(new AudioBook("My Fake Problems", "David Spade", "1 hour 4 minutes", "May 2014"));
+        audioBooks.add(new AudioBook("Lies Chelsea Handler Told Me", "Various Readers", "6 hours 46 minutes", "May 2011"));
+
+    }
+
+    // Get the audiobooks
+    public ArrayList<AudioBook> getAudioBooks() {
+        addAudiobooks();
+        return audioBooks;
     }
 
 }
--- app/src/main/java/com/example/android/musicalstructureapp/Horror.java
+++ app/src/main/java/com/example/android/musicalstructureapp/Horror.java
@@ -8,23 +8,16 @@ import java.util.ArrayList;
 
 public class Horror extends AppCompatActivity {
 
+    // Create private list of audiobooks
+    private ArrayList<AudioBook> audioBooks = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.audiobook_list_view);
 
-        // Create a list of audiobooks
-        ArrayList<AudioBook> audioBooks = new ArrayList<>();
-
         //Add audiobooks
-        audioBooks.add(new AudioBook("The Crooked Staircase", "Elisabeth Rodgers", "15 hours 37 minutes", "May 2018"));
-        audioBooks.add(new AudioBook("Unwelcome Guests", "Cat Gould", "6 hours 41 minutes", "June 2018"));
-        audioBooks.add(new AudioBook("Watchers", "Edoardo Ballerini", "15 hours 22 minutes", "May 2018"));
-        audioBooks.add(new AudioBook("The Director", "George Guidall", "13 hours 34 minutes", "June 2014"));
-        audioBooks.add(new AudioBook("A Steep Price", "Emily Sutton-Smith", "10 hours 10 minutes", "June 2018"));
-        audioBooks.add(new AudioBook("I Am Watching You", "Elizabeth Knowelden", "8 hours 24 minutes", "October 2017"));
-        audioBooks.add(new AudioBook("Killman Creek", "Dan John Miller, Emily Sutton-Smith, Lauren Ezzo, Will Ropp", "11 hours 56 minutes", "December 2017"));
-        audioBooks.add(new AudioBook("An Unwanted Guest", "Hillary Huber", "8 hours 27 minutes", "July 2018"));
+        addAudiobooks();
 
         // Create instance AudioBook adapter with the ArrayList and context
         AudioBookAdapter myAdapter = new AudioBookAdapter(this, audioBooks);
@@ -35,5 +28,24 @@ public class Horror extends AppCompatActivity {
         // MAke use of the adapter in your ListView instance
         listView.setAdapter(myAdapter);
 
+    }
+
+    private void addAudiobooks() {
+
+        audioBooks.add(new AudioBook("The Crooked Staircase", "Elisabeth Rodgers", "15 hours 37 minutes", "May 2018"));
+        audioBooks.add(new AudioBook("Unwelcome Guests", "Cat Gould", "6 hours 41 minutes", "June 2018"));
+        audioBooks.add(new AudioBook("Watchers", "Edoardo Ballerini", "15 hours 22 minutes", "May 2018"));
+        audioBooks.add(new AudioBook("The Director", "George Guidall", "13 hours 34 minutes", "June 2014"));
+        audioBooks.add(new AudioBook("A Steep Price", "Emily Sutton-Smith", "10 hours 10 minutes", "June 2018"));
+        audioBooks.add(new AudioBook("I Am Watching You", "Elizabeth Knowelden", "8 hours 24 minutes", "October 2017"));
+        audioBooks.add(new AudioBook("Killman Creek", "Dan John Miller, Emily Sutton-Smith, Lauren Ezzo, Will Ropp", "11 hours 56 minutes", "December 2017"));
+        audioBooks.add(new AudioBook("An Unwanted Guest", "Hillary Huber", "8 hours 27 minutes", "July 2018"));
+
+    }
+
+    // Get the audiobooks
+    public ArrayList<AudioBook> getAudioBooks() {
+        addAudiobooks();
+        return audioBooks;
     }
 }
